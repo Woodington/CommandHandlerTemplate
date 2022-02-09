460104abf7ea6108f23293845e7c88206089672b
Woodington <93148433+Woodington@users.noreply.github.com>
2022-02-09 23:13:29 +0000

    Tidied comments, minor edit to startup log

--- index.js
+++ index.js
@@ -3,35 +3,35 @@ const { Client, Intents } = require('discord.js');
 const client = new Client({ intents: [30211] }); 
 
 
-const config = require('./config/config.json') //Declars Config File
+const config = require('./config/config.json') 
 
-const token = config.token; //Decalares Token
+const token = config.token; 
 
-const fs = require("fs"); //Declares Filesystem 
+const fs = require("fs");
 
-const prefix = config.prefix; //Prefix (config file to edit :))
+const prefix = config.prefix; 
 
-const commandFiles = fs.readdirSync("./commands").filter(file => file.endsWith(".js"))  //Lays down framework for command handler
-client.commands = new Discord.Collection(); //Lays down framework for command handler
-for(const file of commandFiles) { //Lays down framework for command handler
-    const command = require(`./commands/${file}`); //Lays down framework for command handler
+const commandFiles = fs.readdirSync("./commands").filter(file => file.endsWith(".js"))  
+client.commands = new Discord.Collection(); 
+for(const file of commandFiles) { 
+    const command = require(`./commands/${file}`); 
 
-    client.commands.set(command.name, command); //Lays down framework for command handler
+    client.commands.set(command.name, command); 
 } 
 
-client.once('ready', () => {  //Sends a message to console when the bot starts
-    console.log('Ready'); //Sends a message to console when the bot starts
-});  //Sends a message to console when the bot starts 
+client.once('ready', () => {  
+    console.log(`Ready - Logged in as ${client.user.username}`); 
+});  
 
 client.on("messageCreate", (message) => { 
-    if(!message.content.startsWith(prefix) || message.author.bot) return; //Checks if 1) Command doesnt start with prefix, 2) If the command is sent by a bot. If either are true will not go through
+    if(!message.content.startsWith(prefix) || message.author.bot) return; 
 
     const args = message.content.slice(prefix.length).split(/ +/);
-    const command = args.shift().toLowerCase() //Makes the command not case senstive
+    const command = args.shift().toLowerCase() 
 
     if(command === "ping") { //test command
-        client.commands.execute("ping").run(client, message, args); //test command
-    } //test command
+        client.commands.execute("ping").run(client, message, args); 
+    } /
 });    
 
 client.login(token); 
